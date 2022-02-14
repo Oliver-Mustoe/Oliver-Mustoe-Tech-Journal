517f994120b2deb62880475aae12be4d7063ecc0
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-02-14 22:44:01 +0000

    polishing

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -16,10 +16,10 @@ then
 	# Create the user and it's dependencies
 	sudo useradd -m -d /home/$USERNAME -s /bin/bash $USERNAME
 else
-	echo "$USERNAME already exists, skipping to configuration"
+	echo "$USERNAME already exists, skipping user creation"
 fi
 
-# Make a directory for ssh ("-p" so it already exists skip)
+# Make a directory for ssh ("-p" so if it already exists skip)
 sudo mkdir -p /home/$USERNAME/.ssh
 
 # Copy over the id_rsa.pub key (overwrites each time, but if this key is changed then this is beneficial and needed)
@@ -34,3 +34,14 @@ sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 # Send a message that the process is complete!!!
 echo "Done :)"
+
+#Sources:
+#https://www.cyberciti.biz/faq/linux-check-existing-groups-users/
+#https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+#https://www.tutorialspoint.com/unix/unix-using-variables.htm
+#https://stackoverflow.com/questions/5615717/how-can-i-store-a-command-in-a-variable-in-a-shell-script
+#https://stackoverflow.com/questions/4651437/how-do-i-set-a-variable-to-the-output-of-a-command-in-bash
+#https://stackoverflow.com/questions/26600326/how-to-restart-some-progress-which-is-stopped-by-ctrlz
+#https://superuser.com/questions/118781/copy-files-in-linux-avoid-the-copy-if-files-do-exist-in-destination
+#https://pubs.opengroup.org/onlinepubs/009695399/utilities/mkdir.html
+
