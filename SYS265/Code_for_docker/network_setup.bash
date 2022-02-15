4b4294f704604d67226cf6202c2d6f5311aa89f5
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-14 22:39:28 -0500

    Delete SYS265/Code_for_docker directory

--- SYS265/Code_for_docker/network_setup.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Create a network for the two containers to function in, connect them to it
-
-# Creates network
-docker network create webdata-network
-
-# Adds containers to the network (each container can no reference the other with the hostnames)
-docker network connect webdata-network postgresql
-docker network connect webdata-network apache
