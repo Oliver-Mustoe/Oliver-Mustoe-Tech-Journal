cac55e35ecc28e15dbab7135c17750d808de75de
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-15 20:06:32 -0500

    Add files via upload

--- SYS265/Code_for_docker_project/network_setup.bash
+++ SYS265/Code_for_docker_project/network_setup.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
+# 3
 # Create a network for the two containers to function in, connect them to it
-
 # Creates network
 docker network create webdata-network
 
-# Adds containers to the network (each container can no reference the other with the hostnames)
+# Adds containers to the network (each container can now reference the other with the hostnames)
 docker network connect webdata-network postgresql
 docker network connect webdata-network apache
--- SYS265/Code_for_docker_project/postgresql_setup.bash
+++ SYS265/Code_for_docker_project/postgresql_setup.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
-# First, get the container id of the postgresql container!!!!
+# 4
+# Must be in the same directory as .csv file
 # Must have: run dockerproj_dirmaker.bash on the home directory, have run docker-compose in the projects directory, and have run
+
+# Copy .csv file into needed directory
+cp class_info.csv ~/dockerproject/var/lib/postgresql/data
 
 # Find the id of the container
 ID_DOCK=$(docker ps -aqf "name=^postresql$")
@@ -12,14 +16,13 @@ ID_DOCK=$(docker ps -aqf "name=^postresql$")
 
 #docker exec $ID_DOCK '<COMMAND_GOES_HERE>'
 
-# Create a table inside postgresql
+# Create a table inside postgresql named "DataToDisplay"
 docker exec $ID_DOCK 'CREATE TABLE DataToDisplay (id SERIAL,Class_Number VARCHAR(50),Class VARCHAR(50),Class_Time VARCHAR(50),PRIMARY KEY (id))'
-# Specify where to copy a file to
+# Specify what table is being copied to
 docker exec $ID_DOCK 'COPY DataToDisplay'
+# Specify what directory to copy the file from
+docker exec $ID_DOCK 'FROM '/var/lib/postgresql/data''
 # Specify delimiter
 docker exec $ID_DOCK 'DELIMITER ',''
 # Specify CSV clause
 docker exec $ID_DOCK 'CSV HEADER;'
-
-# WIP: How to get the .csv file into the container, what is setup in .yml (add in the FROM command)?
-# https://www.postgresqltutorial.com/import-csv-file-into-posgresql-table/
