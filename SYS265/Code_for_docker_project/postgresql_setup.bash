5c5ee6b4aecdead4072a507b95ce008c97c60c17
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-16 12:23:06 -0500

    Add files via upload

--- SYS265/Code_for_docker_project/postgresql_setup.bash
+++ SYS265/Code_for_docker_project/postgresql_setup.bash
@@ -7,22 +7,24 @@
 cp class_info.csv ~/dockerproject/var/lib/postgresql/data
 
 # Find the id of the container
-ID_DOCK=$(docker ps -aqf "name=^postresql$")
+ID_DOCK=$(docker ps -aqf "name=^postgresql$")
 #-q=only id output
 #-a=all, even if the container isn't running
 #-f=filter.
 #^=must start here
 #$=must end here
 
-#docker exec $ID_DOCK '<COMMAND_GOES_HERE>'
+#docker exec -it $ID_DOCK '<COMMAND_GOES_HERE>'
 
+# Connect to database
+docker exec -it $ID_DOCK '\c testDB'
 # Create a table inside postgresql named "DataToDisplay"
-docker exec $ID_DOCK 'CREATE TABLE DataToDisplay (id SERIAL,Class_Number VARCHAR(50),Class VARCHAR(50),Class_Time VARCHAR(50),PRIMARY KEY (id))'
+docker exec -it $ID_DOCK 'CREATE TABLE DataToDisplay (id SERIAL,Class_Number VARCHAR(50),Class VARCHAR(50),Class_Time VARCHAR(50),PRIMARY KEY (id))'
 # Specify what table is being copied to
-docker exec $ID_DOCK 'COPY DataToDisplay'
+docker exec -it $ID_DOCK 'COPY DataToDisplay'
 # Specify what directory to copy the file from
-docker exec $ID_DOCK 'FROM '/var/lib/postgresql/data''
+docker exec -it $ID_DOCK 'FROM '/var/lib/postgresql/data''
 # Specify delimiter
-docker exec $ID_DOCK 'DELIMITER ',''
+docker exec -it $ID_DOCK 'DELIMITER ',''
 # Specify CSV clause
-docker exec $ID_DOCK 'CSV HEADER;'
+docker exec -it $ID_DOCK 'CSV HEADER;'
