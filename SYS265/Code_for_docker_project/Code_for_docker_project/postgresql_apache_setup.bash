968cacdaaace5f7de06b76176f577036fcebc02d
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-16 14:45:02 -0500

    Add files via upload

---
+++ SYS265/Code_for_docker_project/Code_for_docker_project/postgresql_apache_setup.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 4
+# Must be in the same directory as .csv file
+# Must have: run dockerproj_dirmaker.bash on the home directory, have run docker-compose in the projects directory, and have run
+
+# Copy .csv file to the postgresql container
+docker cp ./class_info.csv postgresql:/var/lib/postgresql/data/class_info.csv
+
+# Run the contents of the  .sql file against the postgresql container
+cat ./class_to_table.sql | docker exec -i postgresql psql -U postgres -d testDB
+# The .sql file will:
+# Create a table inside postgresql named "DataToDisplay"
+# Specify what table is being copied to
+# Specify what directory to copy the file from
+# Specify delimiter
+# Specify CSV clause
+
+# Copy .php file to the apache container
+docker cp ./index.php apache:/var/www/html/index.php
+
+# Open port for apache
+sudo ufw allow 1492/tcp
