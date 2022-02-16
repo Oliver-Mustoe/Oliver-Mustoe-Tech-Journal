a4ad9f99b98579ce278db64220fa7b44ee300845
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-16 14:45:15 -0500

    Delete SYS265/Code_for_docker_project/Code_for_docker_project directory

--- SYS265/Code_for_docker_project/Code_for_docker_project/class_to_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE DataToDisplay (
-    id SERIAL,
-    Class_Number VARCHAR(50),
-    Class VARCHAR(50),
-    Class_Time VARCHAR(50),
-    PRIMARY KEY (id)
-)
-\copy DataToDisplay(Class_Number,Class,Class_Time) FROM '/var/lib/postgresql/data/class_info.csv' WITH DELIMITER ',' CSV
