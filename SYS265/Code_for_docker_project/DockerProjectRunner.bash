fa5821da952f2b854457e2576c3f193e605d4b3a
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-02-18 18:26:41 -0500

    Add files via upload

--- SYS265/Code_for_docker_project/DockerProjectRunner.bash
+++ SYS265/Code_for_docker_project/DockerProjectRunner.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 # 1-3
 # This script completes the project (making use of dockerproj_dirmaker.bash, docker-compose.yml, and postgresql_apache_setup.bash)
-
+# Waits for scrip flow
 # Runs dockerproj_dirmaker.bash
 bash dockerproj_dirmaker.bash
+wait
 # Moves to the right directory (~/dockerproject)
 cd ~/dockerproject
+wait
 # Runs docker-compose (with sudo)
 sudo docker-compose up -d
+wait
 # Runs postgresql_apache_setup
 bash postgresql_apache_setup.bash
