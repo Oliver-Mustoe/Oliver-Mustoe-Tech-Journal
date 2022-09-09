37f4f772773da59884cee0f98717e4a1b5889958
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-09-09 09:57:32 -0400

    Sources

--- SEC-335/Lab 2.1/portscanner.sh
+++ SEC-335/Lab 2.1/portscanner.sh
@@ -50,3 +50,8 @@ else
 	echo "Need 2 inputs; current inputs are $1 and $2"
 fi # End of if for parameters
 
+# Sources Used:
+# https://stackoverflow.com/questions/14840953/how-to-remove-a-character-at-the-end-of-each-line-in-unix
+# https://opensource.com/article/18/5/you-dont-know-bash-intro-bash-arrays
+# https://linuxhint.com/bash_append_array/
+# https://linuxhint.com/bash_lowercase_uppercase_strings/
