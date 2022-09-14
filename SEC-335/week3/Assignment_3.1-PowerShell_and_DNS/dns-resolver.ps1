da792f4aaa512af39ce3cf156fc348224b505817
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-09-14 14:08:03 -0400

    dns-resolver sources 2

--- SEC-335/week3/Assignment_3.1-PowerShell_and_DNS/dns-resolver.ps1
+++ SEC-335/week3/Assignment_3.1-PowerShell_and_DNS/dns-resolver.ps1
@@ -22,3 +22,6 @@ for ($prefix=1;$prefix -lt 255;$prefix++){
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_for?view=powershell-7.2
 # https://stackoverflow.com/questions/48216173/how-can-i-use-a-shebang-in-a-powershell-script
 # https://docs.microsoft.com/en-us/answers/questions/92154/how-to-check-a-value-is-that-null-in-powershell-sc.html
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object?view=powershell-7.2
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.2
+# https://devblogs.microsoft.com/scripting/powertip-customize-table-headings-with-powershell/
