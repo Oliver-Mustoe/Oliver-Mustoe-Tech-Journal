377fb7458f09e653aec1baa7785917d9ad5d0905
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-09-14 13:42:53 -0400

    dns-resolver

--- SEC-335/week3/Assignment_3.1-PowerShell_and_DNS/dns-resolver.ps1
+++ SEC-335/week3/Assignment_3.1-PowerShell_and_DNS/dns-resolver.ps1
@@ -2,14 +2,17 @@
 
 # Take network prefix and dns server
 # Example run: .\dns-resolver.ps1 192.168.3 192.168.4.5
-
-$netprefix=$args[0]
-$dns=$args[1]
-echo "$netprefix $dns"
+param($netprefix,$dns)
 
 for ($prefix=1;$prefix -lt 255;$prefix++){
+    # Get full IP
+    $IP="$netprefix.$prefix"
+    # Find the name, will throwout errors, use "Select-Object -ExpandProperty Namehost" to just get the name
+    $DNS_Name=Resolve-DnsName -DnsOnly $IP -Server $dns -ErrorAction Ignore | Select-Object -ExpandProperty Namehost
 
-Resolve-DnsName -DnsOnly $netprefix.$prefix -Server $dns -ErrorAction Ignore 
+    # If there is something in $DNS_Name, aka there was no errors in the above command
+    if ($DNS_Name -ne $null){
+    echo "$IP  $DNS_Name"
+    }# End of If statement
 
-# |Select-Object Name, Namehost | Format-Table @{L='Reverse-Query';E={$_.Name}}, @{L='DNS_Name';E={$_.Namehost}}
 }# End of for loop
