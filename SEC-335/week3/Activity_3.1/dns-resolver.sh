4dcabfb00f848ac34d57fcffa608fcb10167cd5f
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-09-14 11:50:40 -0400

    touch ups 2

--- SEC-335/week3/Activity_3.1/dns-resolver.sh
+++ SEC-335/week3/Activity_3.1/dns-resolver.sh
@@ -3,14 +3,17 @@
 netprefix=$1
 dns_server=$2
 
+# Check if variables are empty
 if [[ -n $netprefix ]] && [[ -n $dns_server ]]; then
 	echo "DNS resolution for $netprefix"
+	# For every number between 1-254
 	for prefix in $(seq 1 254); do
-		nslookup $netprefix.$prefix $dns_server 2>/dev/null | egrep '^[[:digit:]]' 
+		# nslookup for variables combined to IP, throw errors in /dev/nul
+		nslookup $netprefix.$prefix $dns_server 2>/dev/null | egrep '^[[:digit:]]' # Checks if digit starts line 
 	done # end of prefix loop
 
 	echo "
-	Done"
+DONE"
 else
 	echo "NEED 2 INPUTS, CURRENT INPUTS ARE: $1 and $2!"
 fi # end of non-zero check
