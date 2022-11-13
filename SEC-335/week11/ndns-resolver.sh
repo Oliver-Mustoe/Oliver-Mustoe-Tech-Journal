44cfcb95b99a961b7deb0305519c82f85e1ca024
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-11-13 11:32:17 -0500

    Week 11

---
+++ SEC-335/week11/ndns-resolver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+netprefix=$1
+dns_server=$2
+
+# Check if variables are empty
+if [[ -n $netprefix ]] && [[ -n $dns_server ]]; then
+	echo "DNS resolution for $netprefix"
+	# For every number between 1-254
+	for prefix in $(seq 1 254); do
+		# nslookup for variables combined to IP, throw errors in /dev/nul
+		nslookup $netprefix.$prefix $dns_server 2>/dev/null | egrep '^[[:digit:]]' # Checks if digit starts line 
+	done # end of prefix loop
+
+	echo "
+DONE"
+else
+	echo "NEED 2 INPUTS, CURRENT INPUTS ARE: $1 and $2!"
+	echo "
+Example input: dns-resolver 10.0.5 10.0.5.22
+"
+fi # end of non-zero check
