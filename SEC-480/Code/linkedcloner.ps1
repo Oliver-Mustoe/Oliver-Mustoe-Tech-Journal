a1354ce8365a20b0f68592f5854b5da57b00b3ec
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2023-02-02 21:20:55 -0500

    linkedcloner-1

---
+++ SEC-480/Code/linkedcloner.ps1
@@ -0,0 +1,35 @@
+param(
+    [Parameter(HelpMessage="Name of VM on Server to clone")]
+    [string]$VMName = "",
+
+    [Parameter(HelpMessage="Name for the linked clone")]
+    [string]$CloneVMName = ""
+)
+
+$default=Get-Content .\defaults.json -Raw | ConvertFrom-Json
+
+$viConnect = Connect-VIServer -Server $default.vcenter -Credential (Get-Credential -Message "Please enter credentials to access $vcenter")
+
+# If one or none of the parameters is set then...
+if ($VMName -eq "" -or $CloneVMName -eq "") {
+    # Display all of the VMs, prompt user to select one by name, also get the new VM name
+    Get-VM
+    $VMName=Read-Host -Prompt "Please enter a the name of the VM to clone"
+    $CloneVMName=Read-Host -Prompt "Please enter the name for the new linked clone"
+}
+
+# Get the VM, Snapshot, VMHost, Datastore (just going to program 1 for now), and the linked name
+$vm = Get-VM -Name $VMName -Server $viConnect
+$snapshot = Get-Snapshot -VM $vm -Name "Base" -Server $viConnect
+$vmhost=Get-VMHost -Name $default.host -Server $viConnect
+$ds = Get-DataStore -Name $default.datastore -Server $viConnect
+
+Write-Host "[Creating $linkedClone]" -ForegroundColor Green
+# Create a new linked clone
+$linkedvm = New-VM -LinkedClone -Name $CloneVMName -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds -Server $viConnect
+
+# Set network adapter properly
+Write-Host "[Setting $linkedClone network adapter to $default.adapter]" -ForegroundColor Green
+$linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $default.adapter -Confirm:$false
+
+Write-Host "[DONE]" -ForegroundColor Green
---
+++ SEC-480/Code/sources.md
@@ -0,0 +1,9 @@
+# Sources for various code
+
+## cloner.ps1
+1. https://powercli-core.readthedocs.io/en/latest/cmd_remove.html?highlight=remove#remove-vm
+2. https://jmmv.dev/2020/10/powershell-cmdlet-params.html
+3. https://developer.vmware.com/apis
+4. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-credential?view=powershell-7.3
+5. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_logical_operators?view=powershell-7.3
+6. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-host?view=powershell-7.3
