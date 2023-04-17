73a4eace5a2da7e2ea183b5e6bff6d0df6f08295
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-04-17 19:41:00 -0400

    ready to test pog

--- SEC-480/ansible/files/windows/drive-maker.ps1
+++ SEC-480/ansible/files/windows/drive-maker.ps1
@@ -4,7 +4,10 @@
 $gpo = New-GPO -Name MappedDrives -Comment "GPO to create groups mapped drives"
 $gpo | new-gplink -target "OU=blue1,DC=blue1,DC=local"
 
-$gpouid = '{}' + $gpo.ToString().ToUpper + '}'
+# Make the GPO path
+$gpouid = '{' + $gpo.Id.ToString().ToUpper() + '}'
+$GpoPath = "\\blue1.local\SYSVOL\blue1.local\Policies\{" + $gpouid + "}"
+
 # Get a ACL object with right permissions and owner/group (blue1 references would be changed for different domain)
 # https://serverfault.com/questions/185192/is-there-a-way-to-create-acls-from-scratch-in-powershell-as-opposed-to-copying
 $empty = New-Object System.Security.AccessControl.DirectorySecurity
@@ -31,28 +34,52 @@ $empty.SetOwner($owner)
 $empty.SetGroup($group)
 
 # Create the Drives folder and Drives.xml file (right permissions in acl) in the GPOs Users\Preference directory (blue1 references would be changed for different domain)
-$DriveFolder = mkdir "\\blue1.local\SYSVOL\blue1.local\Policies\" + $gpouid + "User\Drives"
+
+$DriveFolder = mkdir $GpoPath + "\User\Drives"
 $DriveFolder | Set-Acl -AclObject $empty
-$DriveXml = new-item "\\blue1.local\SYSVOL\blue1.local\Policies\" + $gpouid + "User\Drive\Drives.xml"
+$DriveXml = new-item $GpoPath + "\User\Drives\Drives.xml"
 $DriveXml | Set-Acl -AclObject $empty
+$BasicString = @'
+<?xml version="1.0" encoding="utf-8"?>
+<Drives clsid="{8FDDCC1A-0C3C-43cd-A6B4-71A6DF20DA8C}">
+</Drives>
+'@
+
+Write-host $BasicString > $GpoPath + "\User\Drives\Drives.xml"
 
 
 # Create a hashtable of the desired groups and their ssid's (https://stackoverflow.com/questions/3740128/pscustomobject-to-hashtable)
 https://activedirectorypro.com/get-adgroup-examples/
 $adgroups = Get-ADGroup -filter * -SearchBase "OU=Groups,OU=Accounts,OU=blue1,DC=blue1,DC=local" | select Name, SID
-$GroupDrives = @{}
-$adgroups | Foreach-Object { $GroupDrives[$_.Name] = $_.Value}
+# $GroupDrives = @{}
+# $adgroups | Foreach-Object { $GroupDrives[$_.Name] = $_.SID}
 
 # Have the string that needs to be inputed, with variables where items need to be
-$MapString = ""
+$MapString = ''
 
-# For every group in the GroupDrives
-foreach ($group in $GroupDrives){
-    # Create a uid
-    $uid = [guid]::NewGuid()
-
-    # Create a version of the string
-    $GroupString = ""
-
-    # Append the string to the second to last line in the Drives.xml file
+$adgroups | Foreach-Object {
+    $name = $_.Name
+    $uid = [guid]::NewGuid().ToString().ToUpper()
+    $sid = $_.SID
+    $datetime = (get-date).ToString("yyyy-MM-dd HH:mm:ss")
+    $GroupString = '<Drive clsid="{935D1B74-9CB8-4e3c-9914-7DD559B7A417}" name="Z:" status="Z:" image="2" changed="' + $datetime + '" uid="{ + $uid + }" userContext="1" bypassErrors="1"><Properties action="U" thisDrive="NOCHANGE" allDrives="NOCHANGE" userName="" path="\\fs-blue1\' + $name + ' share" label="" persistent="0" useLetter="1" letter="Z"/><Filters><FilterGroup bool="AND" not="0" name="BLUE1\' + $name +'" sid="' + $sid +'" userContext="1" primaryGroup="0" localGroup="0"/></Filters></Drive>`n'  # `n to indicate newline
+    
+    $MapString += $GroupString
 }
+
+#https://stackoverflow.com/questions/31957901/add-content-append-to-specific-line
+$FileContent = Get-Content -Path $GpoPath + "\User\Drives\Drives.xml"
+$FileContent[-1] = "{0}`r`n{1}" -f $fileContent[-1],$test_out
+
+$FileContent | Set-Content $GpoPath + "\User\Drives\Drives.xml"
+
+# # For every group in the GroupDrives
+# foreach ($group in $GroupDrives){
+#     # Create a uid
+#     $uid = [guid]::NewGuid()
+
+#     # Create a version of the string
+#     $GroupString = ""
+
+#     # Append the string to the second to last line in the Drives.xml file
+# }
