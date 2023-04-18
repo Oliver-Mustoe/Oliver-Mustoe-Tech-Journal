687a33bb57e538eea15869043c10910ebce45286
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-04-17 20:10:15 -0400

    test 4

--- SEC-480/ansible/files/windows/drive-maker.ps1
+++ SEC-480/ansible/files/windows/drive-maker.ps1
@@ -1,7 +1,7 @@
 # makes the groups mapped drives
 
 # Set gpo name (doubles as a funny counter for how many times I have tried this script :))
-$GpoName = "MappedDrives3"
+$GpoName = "MappedDrives4"
 # Create the GPO
 Write-host "[Creating $GpoName]"
 $gpo = New-GPO -Name $GpoName -Comment "GPO to create groups mapped drives"
@@ -9,7 +9,7 @@ $gpo | new-gplink -target "OU=blue1,DC=blue1,DC=local"
 
 # Make the GPO path
 $gpouid = '{' + $gpo.Id.ToString().ToUpper() + '}'
-$GpoPath = "\\blue1.local\SYSVOL\blue1.local\Policies\{" + $gpouid + "}"
+$GpoPath = "\\blue1.local\SYSVOL\blue1.local\Policies\$gpouid"
 
 # Get a ACL object with right permissions and owner/group (blue1 references would be changed for different domain)
 # https://serverfault.com/questions/185192/is-there-a-way-to-create-acls-from-scratch-in-powershell-as-opposed-to-copying
@@ -55,7 +55,7 @@ $BasicString = @'
 '@
 
 Write-host "[Setting Drives.xml basic structure]"
-Write-host $BasicString > "$GpoPath\User\Drives\Drives.xml"
+$BasicString > "$GpoPath\User\Drives\Drives.xml"
 
 
 # Create a hashtable of the desired groups and their ssid's (https://stackoverflow.com/questions/3740128/pscustomobject-to-hashtable)
@@ -69,13 +69,12 @@ $adgroups = Get-ADGroup -filter * -SearchBase "OU=Groups,OU=Accounts,OU=blue1,DC
 $MapString = ''
 
 $adgroups | Foreach-Object {
-    Write-host "[Creating $names's entry]"
     $name = $_.Name
     $uid = [guid]::NewGuid().ToString().ToUpper()
     $sid = $_.SID
     $datetime = (get-date).ToString("yyyy-MM-dd HH:mm:ss")
     $GroupString = '<Drive clsid="{935D1B74-9CB8-4e3c-9914-7DD559B7A417}" name="Z:" status="Z:" image="2" changed="' + $datetime + '" uid="{' + $uid + '}" userContext="1" bypassErrors="1"><Properties action="U" thisDrive="NOCHANGE" allDrives="NOCHANGE" userName="" path="\\fs-blue1\' + $name + ' share" label="" persistent="0" useLetter="1" letter="Z"/><Filters><FilterGroup bool="AND" not="0" name="BLUE1\' + $name +'" sid="' + $sid +'" userContext="1" primaryGroup="0" localGroup="0"/></Filters></Drive>`n'  # `n to indicate newline
-    
+    Write-host "[Creating $names's entry]"
     $MapString += $GroupString
 }
 
