8b429592a1b38cafd6285a4ac1b7f6f7c1593732
Oliver-Mustoe <oliver.mustoe@mymail.champlain.edu>
2023-04-17 22:05:56 -0400

    NEED TO TEST ON FRESH ENVIRO - BUT IS WORKING RN!

--- SEC-480/ansible/files/windows/drive-maker.ps1
+++ SEC-480/ansible/files/windows/drive-maker.ps1
@@ -1,15 +1,15 @@
-# makes the groups mapped drives
+# makes the groups mapped drives - working - cleanup and test on a fresh enviro
 
 # Set gpo name (doubles as a funny counter for how many times I have tried this script :))
-$GpoName = "MappedDrives5"
+$GpoName = "MappedDrives15"
 # Create the GPO
 Write-host "[Creating $GpoName]"
 $gpo = New-GPO -Name $GpoName -Comment "GPO to create groups mapped drives"
-$gpo | new-gplink -target "OU=blue1,DC=blue1,DC=local"
+new-gplink -Name $gpo.DisplayName -target "OU=blue1,DC=blue1,DC=local"
 
 # Make the GPO path
-# $gpouid = '{' + $gpo.Id.ToString().ToUpper() + '}'
-$gpouid = $gpo.Id.ToString().ToUpper()
+$gpouid = '{' + $gpo.Id.ToString().ToUpper() + '}'
+# $gpouid = $gpo.Id.ToString().ToUpper()
 $GpoPath = "\\blue1.local\SYSVOL\blue1.local\Policies\$gpouid"
 
 # Get a ACL object with right permissions and owner/group (blue1 references would be changed for different domain)
@@ -18,45 +18,49 @@ Write-host "[Creating blank ACL]"
 $empty = New-Object System.Security.AccessControl.DirectorySecurity
 
 # https://blog.netwrix.com/2018/04/18/how-to-manage-file-system-acls-with-powershell-scripts/
-$AccessRule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("BLUE1\Domain Admins","FullControl","Allow")
-$AccessRule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("BLUE1\Enterprise Admins","FullControl","Allow")  # User creating the GPO is a enterprise admin so this MAY affect this in a diff enviro!
-$AccessRule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\ENTERPRISE DOMAIN CONTROLLERS","ReadAndExecute","Allow")
-$AccessRule4 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users","ReadAndExecute","Allow")
-$AccessRule33 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\ENTERPRISE DOMAIN CONTROLLERS","Synchronize","Allow")
-$AccessRule44 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users","Synchronize","Allow")
-$AccessRule5 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM","FullControl","Allow")
-$AccessRule6 = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators","FullControl","Allow")
+# $AccessRule1 = New-Object System.Security.AccessControl.FileSystemAccessRule("BLUE1\Domain Admins","FullControl","Allow")
+# $AccessRule2 = New-Object System.Security.AccessControl.FileSystemAccessRule("BLUE1\Enterprise Admins","FullControl","Allow")  # User creating the GPO is a enterprise admin so this MAY affect this in a diff enviro!
+# $AccessRule3 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\ENTERPRISE DOMAIN CONTROLLERS","ReadAndExecute","Allow")
+# $AccessRule4 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users","ReadAndExecute","Allow")
+# $AccessRule33 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\ENTERPRISE DOMAIN CONTROLLERS","Synchronize","Allow")
+# $AccessRule44 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users","Synchronize","Allow")
+# $AccessRule5 = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM","FullControl","Allow")
+# $AccessRule6 = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators","FullControl","Allow")
 $AccessRule7 = New-Object System.Security.AccessControl.FileSystemAccessRule("CREATOR OWNER","FullControl","Allow")
 $owner = New-Object System.Security.Principal.Ntaccount("BUILTIN\Administrators")
 $group = New-Object System.Security.Principal.Ntaccount("BLUE1\Domain Users")
 
-$empty.SetAccessRule($AccessRule1)
-$empty.SetAccessRule($AccessRule2)
-$empty.SetAccessRule($AccessRule3)
-$empty.SetAccessRule($AccessRule4)
-$empty.SetAccessRule($AccessRule33)
-$empty.SetAccessRule($AccessRule44)
-$empty.SetAccessRule($AccessRule5)
-$empty.SetAccessRule($AccessRule6)
+# $empty.SetAccessRule($AccessRule1)
+# $empty.SetAccessRule($AccessRule2)
+# $empty.SetAccessRule($AccessRule3)
+# $empty.SetAccessRule($AccessRule4)
+# $empty.SetAccessRule($AccessRule33)
+# $empty.SetAccessRule($AccessRule44)
+# $empty.SetAccessRule($AccessRule5)
+# $empty.SetAccessRule($AccessRule6)
 $empty.SetAccessRule($AccessRule7)
 $empty.SetOwner($owner)
 $empty.SetGroup($group)
 
-# Create the Drives folder and Drives.xml file (right permissions in acl) in the GPOs Users\Preference directory (blue1 references would be changed for different domain)
+# Create the Drives folder and Drives.xml file (right permissions in acl) in the GPOs Users\Preference directory (blue1 references would be changed for different domain) https://stackoverflow.com/questions/6573308/removing-all-acl-on-folder-with-powershell
 Write-host "[Creating Drives folder]"
-$DriveFolder = mkdir "$GpoPath\User\Drives"
+$DriveFolder = mkdir "$GpoPath\User\Preferences\Drives"
+$folderacl = Get-Acl "$GpoPath\User\Preferences\Drives"
+$folderacl.Access | ForEach-Object{$folderacl.RemoveAccessRule($_)}
 $DriveFolder | Set-Acl -AclObject $empty
 Write-host "Creating Drives.xml"
-$DriveXml = new-item "$GpoPath\User\Drives\Drives.xml"
+$DriveXml = new-item "$GpoPath\User\Preferences\Drives\Drives.xml" -ItemType File
+$xmlacl = Get-Acl "$GpoPath\User\Preferences\Drives"
+$xmlacl.Access | ForEach-Object{$xmlacl.RemoveAccessRule($_)}
 $DriveXml | Set-Acl -AclObject $empty
-$BasicString = @'
-<?xml version="1.0" encoding="utf-8"?>
-<Drives clsid="{8FDDCC1A-0C3C-43cd-A6B4-71A6DF20DA8C}">
-</Drives>
-'@
+# $BasicString = @'
+# <?xml version="1.0" encoding="utf-8"?>
+# <Drives clsid="{8FDDCC1A-0C3C-43cd-A6B4-71A6DF20DA8C}">
+# </Drives>
+# '@
 
-Write-host "[Setting Drives.xml basic structure]"
-$BasicString > "$GpoPath\User\Drives\Drives.xml"
+# Write-host "[Setting Drives.xml basic structure]"
+# $BasicString > "$GpoPath\User\Drives\Drives.xml"
 
 
 # Create a hashtable of the desired groups and their ssid's (https://stackoverflow.com/questions/3740128/pscustomobject-to-hashtable)
@@ -74,18 +78,27 @@ $adgroups | Foreach-Object {
     $uid = [guid]::NewGuid().ToString().ToUpper()
     $sid = $_.SID
     $datetime = (get-date).ToString("yyyy-MM-dd HH:mm:ss")
-    $GroupString = '<Drive clsid="{935D1B74-9CB8-4e3c-9914-7DD559B7A417}" name="Z:" status="Z:" image="2" changed="' + $datetime + '" uid="{' + $uid + '}" userContext="1" bypassErrors="1"><Properties action="U" thisDrive="NOCHANGE" allDrives="NOCHANGE" userName="" path="\\fs-blue1\' + $name + ' share" label="" persistent="0" useLetter="1" letter="Z"/><Filters><FilterGroup bool="AND" not="0" name="BLUE1\' + $name +'" sid="' + $sid +'" userContext="1" primaryGroup="0" localGroup="0"/></Filters></Drive>`n'  # `n to indicate newline
-    Write-host "[Creating $names entry]"
+    # $GroupString = '<Drive clsid="{935D1B74-9CB8-4e3c-9914-7DD559B7A417}" name="Z:" status="Z:" image="2" changed="' + $datetime + '" uid="{' + $uid + '}" userContext="1" bypassErrors="1"><Properties action="U" thisDrive="NOCHANGE" allDrives="NOCHANGE" userName="" path="\\fs-blue1\' + $name + ' share" label="" persistent="0" useLetter="1" letter="Z"/><Filters><FilterGroup bool="AND" not="0" name="BLUE1\' + $name +'" sid="' + $sid +'" userContext="1" primaryGroup="0" localGroup="0"/></Filters></Drive> `r`n'  # `n to indicate newline
+    $GroupString = '<Drive clsid="{935D1B74-9CB8-4e3c-9914-7DD559B7A417}" name="Z:" status="Z:" image="2" changed="' + $datetime + '" uid="{' + $uid + '}" userContext="1" bypassErrors="1"><Properties action="U" thisDrive="NOCHANGE" allDrives="NOCHANGE" userName="" path="\\fs-blue1\' + $name + ' share" label="" persistent="0" useLetter="1" letter="Z"/><Filters><FilterGroup bool="AND" not="0" name="BLUE1\' + $name +'" sid="' + $sid +'" userContext="1" primaryGroup="0" localGroup="0"/></Filters></Drive>' + "`r`n"  # `n to indicate newline
+    Write-host "[Creating $name entry]"
     Write-host "[$sid]"
     $MapString += $GroupString
 }
 
 #https://stackoverflow.com/questions/31957901/add-content-append-to-specific-line
+# Write-Host $MapString
 Write-host "[Setting the xml file to the new entires!]"
-$FileContent = Get-Content -Path "$GpoPath\User\Drives\Drives.xml"
-$FileContent[-1] = "{0}`r`n{1}" -f $fileContent[-1],$test_out
+$FullString = @'
+<?xml version="1.0" encoding="utf-8"?>
+<Drives clsid="{8FDDCC1A-0C3C-43cd-A6B4-71A6DF20DA8C}">
+'@ + "`r`n" + $MapString + '</Drives>'
+# Write-Host $FullString
+Set-Content -Path "$GpoPath\User\Preferences\Drives\Drives.xml" $FullString
 
-$FileContent | Set-Content "$GpoPath\User\Drives\Drives.xml"
+# $FileContent = Get-Content -Path "$GpoPath\User\Drives\Drives.xml"
+# $FileContent[-1] = "{0}`r`n{1}" -f $fileContent[-1],$MapString
+
+# $FileContent | Set-Content "$GpoPath\User\Drives\Drives.xml"
 
 # # For every group in the GroupDrives
 # foreach ($group in $GroupDrives){
