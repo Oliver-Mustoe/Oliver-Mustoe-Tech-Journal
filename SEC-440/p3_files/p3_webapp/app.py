0c29c66550e1be8fe3b056f31851d92cbd422fe6
Olive <71083461+Oliver-Mustoe@users.noreply.github.com>
2023-09-27 18:26:31 -0400

    Update app.py

--- SEC-440/p3_files/p3_webapp/app.py
+++ SEC-440/p3_files/p3_webapp/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector as database
+from socket import gethostname
 import re, hashlib
 
 app = Flask(__name__)
 
 # Secret key
-app.secret_key = '<SET_SECRET_KEY_HERE>'
+app.secret_key = 'SALT'
 
 # Database connection detail
 db_config = {
@@ -24,22 +25,21 @@ db_connection = database.connect(**db_config)
 def login():
     # Output a message if something goes wrong
     msg = ''
+    host = gethostname()
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
+        # Get the password hash
+        password_hash = password+app.secret_key
+        password_hashed = hashlib.sha3_512(password_hash.encode())
+        final_password = password_hashed.hexdigest()
         # Check if account exists using MySQL
         cursor = db_connection.cursor()
-        query = 'SELECT id,username FROM users WHERE username = %(user)s AND password = %(pass)s'
-        #query = 'SELECT * FROM users WHERE username = %s AND password = %s'
-        #query_params = (username,password)
-        query_params = {'user': username, 'pass': password}
-        cursor.execute(query,query_params)
-        cursor._executed
+        cursor.execute('SELECT id,username FROM users WHERE username = %(user)s AND password = %(pass)s', {'user': username, 'pass': final_password})
         # Fetch one record and return result
         account = cursor.fetchone()
-        print(account)
         # If account exists in users table in out database
         if account:
             # Create session data
@@ -47,77 +47,43 @@ def login():
             session['id'] = account[0]
             session['username'] = account[1]
             # Redirect to home page
-            return 'Logged in successfully!'
+            return f"Logged in {account[0]}:'{username}' successfully!"
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
-    return render_template('index.html', msg=msg)
+    return render_template('index.html', msg=msg, host=host)
 
-"""
-Credits:
-https://codeshack.io/login-system-python-flask-mysql/
-https://hackernoon.com/getting-started-with-mariadb-using-docker-python-and-flask-pa1i3ya3
-https://testdriven.io/blog/flask-sessions/
-https://www.w3schools.com/python/gloss_python_function_arbitrary_keyword_arguments.asp
-"""
-(p3_env) [olivermustoe@web01-oliver p3_webapp]$ cat app.py 
-from flask import Flask, render_template, request, redirect, url_for, session
-import mysql.connector as database
-import re, hashlib
-
-app = Flask(__name__)
-
-# Secret key
-app.secret_key = '<SET_SECRET_KEY_HERE>'
-
-# Database connection detail
-db_config = {
-    'host': '10.0.6.10',
-    'port': 3306,
-    'user': 'olivermustoe',
-    'password': '#$Gamingtoday123',
-    'database': 'the_vault'
-}
-
-# Intialize MySQL
-db_connection = database.connect(**db_config)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    # Output a message if something goes wrong
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        # Check if account exists using MySQL
+        
+        # Connect to database, see if account name already exists
         cursor = db_connection.cursor()
-        query = 'SELECT id,username FROM users WHERE username = %(user)s AND password = %(pass)s'
-        #query = 'SELECT * FROM users WHERE username = %s AND password = %s'
-        #query_params = (username,password)
-        query_params = {'user': username, 'pass': password}
-        cursor.execute(query,query_params)
-        cursor._executed
-        # Fetch one record and return result
+        cursor.execute("SELECT username FROM users WHERE username = %(user)s AND password = %(pass)s", {'user': username, 'pass': password})
         account = cursor.fetchone()
-        print(account)
-        # If account exists in users table in out database
-        if account:
-            # Create session data
-            session['loggedin'] = True
-            session['id'] = account[0]
-            session['username'] = account[1]
-            # Redirect to home page
-            return 'Logged in successfully!'
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
-    return render_template('index.html', msg=msg)
 
+        if account:
+            msg = 'Your account already exists!'
+        elif not re.match(r'[A-Za-z0-9]',username):
+           msg = 'Username must only be characters and numbers!' 
+        elif not username or not password:
+           msg = "Missing data from the forum: please fill it out!"
+        else:
+            password_hash = password+app.secret_key
+            password_hashed = hashlib.sha3_512(password_hash.encode())
+            final_password = password_hashed.hexdigest()
+
+            cursor.execute("INSERT INTO users VALUES (NULL, %(user)s, %(hashpass)s)",{'user': username, 'hashpass': final_password})
+            db_connection.commit()
+            msg = 'Successfully registered!' 
+    elif request.method == 'POST':
+        msg = "Missing data from the forum: please fill it out!"
+    return render_template('register.html', msg=msg)
 """
 Credits:
 https://codeshack.io/login-system-python-flask-mysql/
